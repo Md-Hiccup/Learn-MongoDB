c88300eae6b11ae14ed6f3b5ebdb2722ee30f11f
Md-Hiccup <ask2md@gmail.com>
2018-07-25 18:03:44 +0530

    relationship and use of populate

--- users/src/user.js
+++ users/src/user.js
@@ -1,24 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const PostSchema = require('./post');
+const PostSchema = require("./post");
 
 const UserSchema = new Schema({
   name: {
     type: String,
     validate: {
-      validator: (name) => name.length > 2,
-      message: 'Name must be longer than 2 characters'
+      validator: name => name.length > 2,
+      message: "Name must be longer than 2 characters"
     },
-    required: [true, 'Name is required.']
+    required: [true, "Name is required."]
   },
   posts: [PostSchema],
-  likes: Number
+  likes: Number,
+  blogPosts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "blogPost"
+    }
+  ]
 });
 
-UserSchema.virtual('postCount').get(function () {
+UserSchema.virtual("postCount").get(function() {
   return this.posts.length;
-})
+});
 
-const User = mongoose.model('user', UserSchema);    // Collection: user, & Schema: UserSchema
+const User = mongoose.model("user", UserSchema); // Collection: user, & Schema: UserSchema
 
 module.exports = User;
